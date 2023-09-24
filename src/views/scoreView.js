84abb48592897b275639d8d6d94f193b6eab4915
sanaz <sanazzarei1995@gmail.com>
2023-09-24 19:55:39 +0200

    add minor changes

--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -3,11 +3,11 @@
  * @returns {Element}
  */
 export const createScoreElement = (score) =>{
-    const scoreElement = document.createElement('div');
-    scoreElement.innerHTML = `
+    const element = document.createElement('div');
+    element.classList.add('score-element');
+    element.innerHTML = `
     <h3> Your Score </h3>
     <p> ${score} point${score === 1 ? '' : 's'} </p>
     `;
-
-
+    return element;
 }
